530354860f63ab02866923c8f048a7e91e63e12f
Pattarapol <adisus2@MacBook-Air-khxng-Pattarapol.local>
2021-04-12 23:57:28 +0700

    [ADD]Background.vue

---
+++ src/components/Background.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="w-screen h-screen">
+    <video autoplay muted loop id="myVideo">
+      <source :src="require(`@/assets/Nightsky.mp4`)" type="video/mp4" />
+    </video>
+    <slot></slot>
+  </div>
+</template>
+  
+  <script>
+export default {};
+</script>
+  
+  <style>
+.background {
+  background-image: url("../assets/images/Blackhole.jpg");
+}
+
+#myVideo {
+  z-index: -1;
+  position: fixed;
+  right: 0;
+  bottom: 0;
+  min-width: 100%;
+  min-height: 100%;
+}
+</style>
