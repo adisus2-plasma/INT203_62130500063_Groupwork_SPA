05973f9f8d15f1ce33c22bbe214a0caecbfcd8b6
Pattarapol <adisus2@MacBook-Air-khxng-Pattarapol.local>
2021-04-13 00:09:31 +0700

    [UPDATE]Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,31 @@
 <template>
-  <div class="home">
+<div>
+  <Background>
+  <div id="app" class="flex justify-center pt-6 mb-12 mr-8">
+    <img class="logo" src="../assets/images/saturn.png">
+    <h1 class="topic">W I T A R N . T H</h1>
   </div>
+
+
+
+  <Info class="mt-96"></Info>
+
+</Background>
+</div>
+  
 </template>
 
 <script>
-// @ is an alias to /src
-
-
-export default {
-  name: 'Home',
-  components: {
+  import Background from '../components/Background.vue'
+  import Info from '../components/Info.vue'
+  
+  export default {
+    name: 'App',
+    components: {
+      Background,
+      Info
+    }
   }
-}
-</script>
+  
+  
+  </script>
